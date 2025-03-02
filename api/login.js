3d3098b46daf387fe2bcfe68c50ce5c1419d263a
saulocastrolp <saulocastrolp@gmail.com>
2025-03-02 12:48:58 -0300

    Refatorando função de login

--- api/login.js
+++ api/login.js
@@ -1,25 +1,5 @@
 const { google } = require("googleapis");
 
-const oauth2Client = new google.auth.OAuth2(
-    process.env.CLIENT_ID,
-    process.env.CLIENT_SECRET,
-    process.env.REDIRECT_URI
-);
-
-module.exports = (req, res) => {
-    const authUrl = oauth2Client.generateAuthUrl({
-        access_type: "offline",
-        scope: [
-            "https://www.googleapis.com/auth/youtube",
-            "https://www.googleapis.com/auth/youtube.force-ssl",
-        ],
-    });
-    res.redirect(authUrl);
-};
-
-
-const { google } = require("googleapis");
-
 module.exports = async (req, res) => {
     console.log("🔍 [LOGIN] Redirecionando para autenticação do Google...");
 
